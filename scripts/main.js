1d914222fa9465cdda63f030531c1e56a81ae124
Sean Wines <sean.wines@vuria.com>
2017-10-10 11:49:03 -0700

    Build Progress - general layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,28 @@
 $(function () {
+  // Fade in social-toolbar
   $('.social-toolbar').css('right', 0);
 
+  // Fade in email tooltip
+  setTimeout(function () {
+    $('#email-container').css('bottom', 0);
+  }, 2000);
+
+  // Close email tooltip
+  $('.fa-close').on('click', function () {
+    $('#email-container').css('bottom', '-20rem');
+  })
+
+  // truncate blog paragraph
+  $('.column-callout .content p').each(function () {
+    var paragraphLength = 550;
+
+    if ($(this).text().length >  paragraphLength) {
+      var text = $(this).text().substring(0, paragraphLength);
+      $(this).text(text + '...');
+    }
+  })
+
+  // Smooth scroll
   $('a').on('click', function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
         || location.hostname == this.hostname) {
@@ -14,5 +36,5 @@ $(function () {
             return false;
         }
     }
-});
+  });
 })
